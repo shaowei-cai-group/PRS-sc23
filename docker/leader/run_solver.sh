b7e8dbf6a06999110ab246f5efc7ba924614ff5c
YuhangQ <i@yuhangq.com>
2023-05-15 08:12:37 +0000

    增加timeout的设定

--- docker/leader/run_solver.sh
+++ docker/leader/run_solver.sh
@@ -2,4 +2,4 @@
 
 mpirun --mca btl_tcp_if_include eth0 --allow-run-as-root \
     --hostfile $1 --bind-to none --allow-run-as-root \
-    /PRS-distributed -i $2 --share=1 --threads=16 --times=1 --share_method=0
+    /PRS-distributed -i $2 --share=1 --threads=16 --times=$3 --share_method=0
